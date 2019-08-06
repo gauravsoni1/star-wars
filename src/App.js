e06fc0abf3a11e02019f4c5d7749b27026fef202
Gaurav soni <Gaurav@allcadservices.com>
2019-08-06 16:29:42 +0400

    updated the router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import {Route} from "react-router-dom";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 //Components
-import Login from './container/login/login';
-import Search from './container/search/search';
-import PrivateRoute from './components/privateRoute/privateRoute';
+import Login from "./container/login/login";
+import Search from "./container/search/search";
+import PrivateRoute from "./components/privateRoute/privateRoute";
 
 function App() {
-
   return (
     <>
-    <PrivateRoute path="/search" component={Search}/> 
-    <Route path="/login" exact component={Login} />
+      <Switch>
+        <PrivateRoute path="/search" component={Search} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/" component={Login} />
+      </Switch>
     </>
   );
 }
