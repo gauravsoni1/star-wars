9b4750be0e2c53b7af0178a03b27a4892ec192a7
Gaurav soni <Gaurav@allcadservices.com>
2019-08-06 16:02:44 +0400

    Updates

---
+++ precache-manifest.990dfd85e7a6d119674cc9d552b88785.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26a8d5edade710bea670a91171f9258",
+    "url": "/star-wars/index.html"
+  },
+  {
+    "revision": "bd9ed54f8e63fd4911dd",
+    "url": "/star-wars/static/css/main.2081c4b9.chunk.css"
+  },
+  {
+    "revision": "511dd920a1deced11d8c",
+    "url": "/star-wars/static/js/2.999b11de.chunk.js"
+  },
+  {
+    "revision": "bd9ed54f8e63fd4911dd",
+    "url": "/star-wars/static/js/main.7805921e.chunk.js"
+  },
+  {
+    "revision": "aedf8bebf74d2f803586",
+    "url": "/star-wars/static/js/runtime~main.6cf174e3.js"
+  },
+  {
+    "revision": "888edd43cb7c1cbb626caeff977d3a20",
+    "url": "/star-wars/static/media/lightsaber.888edd43.png"
+  },
+  {
+    "revision": "a541675e570f0f7f0b2e8d611a8448b3",
+    "url": "/star-wars/static/media/loginBackground.a541675e.jpg"
+  }
+]);
